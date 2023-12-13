afbcf7d8a13fa1e8c16ad953d4c37e4c208b7586
oscarfdlv <148486910+oscarfdlv@users.noreply.github.com>
2023-12-13 18:35:27 +0100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import LogInUsers
 
 def home(request):
     return render(request, "main/base.html")
@@ -7,3 +7,36 @@ def home(request):
 
 def members(request):
     return render(request, "main/members.html")
+
+def logIn( request ):
+    if ( request.method == 'POST'):
+        form = LogInUsers(request.POST)
+        if ( form.is_valid()):
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if ( user is not None):
+                if ( request.user.is_authenticated ):
+                    logout(request)
+                login(request, user)
+                nextPage=request.GET.get('next')
+                if ( nextPage is None ):
+                    nextPage = reverse(' ') # Completar con el nombre de la pág inicial
+                return redirect (nextPage)
+            else:
+                error='User or password incorrect'
+                return render(request, LOGIN_USUARIOS, {'form':form, 'errorMsg':error })
+        else:
+            error="The data in some form field is incorrect"
+            return render(request, LOGIN_USUARIOS, {'form':form, 'errorMsg':error })
+    else:
+        form = LogInUsers()
+        if ( request.GET.get('error403') is None):
+            error=None
+        else:
+            error='Operation not permitted. Use an account with sufficient permissions'
+        return render(request, LOGIN_USUARIOS, {'form':form, 'errorMsg': error })
+
+def logOut( request ):
+    logout( request )
+    return redirect (' ') # Completar con el nombre de la pág inicial
