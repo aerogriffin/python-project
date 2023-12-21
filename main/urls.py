4688bc15625d56bb2c95c87698c396b58c8264ba
aerogriffin <47277556+aerogriffin@users.noreply.github.com>
2023-12-21 01:11:52 +0100

    Added logout button

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -6,9 +7,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login/", views.login_view, name="login"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path("register/", views.register, name="register"),
     path("upload/", views.upload_video, name="upload_video"),
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("account/profile/", views.account_profile, name="account_profile"),
 ]
