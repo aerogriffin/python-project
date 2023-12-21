45113f22acb7732314bd5ba06d4b27dcb7dde566
aerogriffin <47277556+aerogriffin@users.noreply.github.com>
2023-12-21 01:11:12 +0100

    Reworked the whole login and register logic to use django built-in functions

--- main/forms.py
+++ main/forms.py
@@ -1,38 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
 from django.core.validators import FileExtensionValidator
 
 from .models import Video
-
-
-class LogInUsers(forms.Form):
-    username = forms.CharField(label="User", max_length=10, widget=forms.TextInput(attrs={"placeholder": "user"}))
-    password = forms.CharField(
-        label="Password",
-        max_length=20,
-        widget=forms.PasswordInput(attrs={"placeholder": "password"}),
-    )
-
-
-class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password])
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "password"]
-
-    def clean_username(self):
-        username = self.cleaned_data.get("username")
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("Username is already taken.")
-        return username
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email is already in use.")
-        return email
 
 
 class VideoUploadForm(forms.ModelForm):
