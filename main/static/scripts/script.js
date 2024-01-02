94185a9214d28a049cee33fc2d90d9add89df97b
Marifer Rodriguez <119091615+MaferRod@users.noreply.github.com>
2024-01-02 23:32:20 +0100

    Update script.js

--- main/static/scripts/script.js
+++ main/static/scripts/script.js
@@ -14,3 +14,17 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+function toggleUserOptions() {
+    var userOptions = document.getElementById("userOptions");
+    userOptions.style.display = (userOptions.style.display === "block") ? "none" : "block";
+}
+
+// Cerrar las opciones del usuario al hacer clic fuera de ellas
+document.addEventListener("click", function(event) {
+    var userSection = document.querySelector(".user-section");
+    var userOptions = document.getElementById("userOptions");
+
+    if (event.target !== userSection && !userSection.contains(event.target)) {
+        userOptions.style.display = "none";
+    }
+});
